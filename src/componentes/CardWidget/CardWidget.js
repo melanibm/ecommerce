21f57220d5e8ab875bcb61d1a7b43feb76d5a64e
melanibm <melanimoran.mm@gmail.com>
2023-12-12 00:17:31 -0300

    agregando componentes

--- src/componentes/CardWidget/CardWidget.js
+++ src/componentes/CardWidget/CardWidget.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import cart from './imagenes/cart-outline.svg';
 import '../CardWidget/card-widget.css'
-=======
-import cart from './imagenes/file.jpg'
->>>>>>> a53aefb34bcf74024bd937d38fe8c2cc3be18355
 
 const CartWidget = ({ itemCount }) => {
     return (
-<<<<<<< HEAD
       <div className="cart-widget">
         <img src={cart} alt="cart-widget" />
         <span className="item-count">{itemCount}</span>
@@ -17,13 +12,3 @@ const CartWidget = ({ itemCount }) => {
   };
   
   export default CartWidget;
-=======
-        <div>
-            <img src={cart} alt="cart-widget"/>
-            0
-        </div> 
-    )
-}
-
-export default CartWidget
->>>>>>> a53aefb34bcf74024bd937d38fe8c2cc3be18355
